e2701a9ba60ef5ba8c24877df0e1edf04112c9b4
dmsgo <dmsgod0010@gmail.com>
2025-03-25 06:35:38 +0900

    HW2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,87 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        int[] IDs = new int[10];
+        int[] scores = new int[10];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("10명 학생의 학번과 점수를 입력하세요.");
+
+        for(int i = 0; i<10; i++){
+            System.out.print((i + 1) + " : ");
+            IDs[i] = scanner.nextInt();
+            scores[i] = scanner.nextInt();
         }
+
+        while(true){ // 1 2 3 중 입력받기
+            System.out.print("학번으로 검색은 1, 점수로 검색은 2, 끝내려면 3을 입력하세요. : ");
+            int num = scanner.nextInt();
+
+            if(num == 1){
+                int inputID;
+                while(true){
+                    try{
+                        System.out.print("학번 : ");
+                        inputID = scanner.nextInt();
+                        break;
+                    }
+                    catch(InputMismatchException e){
+                        System.out.println("경고! 정수를 입력하세요.");
+                        scanner.nextLine();
+                    }
+                }
+                boolean search = false;
+
+                for(int i = 0; i < 10; i++){
+                    if(IDs[i] == inputID){
+                        System.out.println(scores[i] + " 점 ");
+                        search = true;
+                        break;
+                    }
+                }
+                if(!search){
+                    System.out.println("학번 " + inputID + " 의 학생은 없습니다.");
+                }
+            }
+
+            else if(num == 2){
+                int inputScore;
+                while(true){
+                    try{
+                        System.out.print("점수: ");
+                        inputScore = scanner.nextInt();
+                        break;
+                    }
+                    catch(InputMismatchException e){
+                        System.out.println("경고! 정수를 입력하세요.");
+                        scanner.nextLine();
+                    }
+                }
+                boolean search = false;
+
+                for(int i = 0; i < 10; i++){ // 점수가 10인 학생은 1 2 3 식이 아니라 문장 n개가 출력됨
+                    if(scores[i] == inputScore){
+                        System.out.println("점수가 " + inputScore + "인 학생은 "+
+                                IDs[i] + " 입니다.");
+                        search = true;
+                    }
+                }
+                if(!search){
+                    System.out.println("점수가 " + inputScore + "인 학생은 없습니다.");
+                }
+            }
+            else if(num == 3){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            else{
+                System.out.println("1 2 3 중에 입력하세요.");
+            }
+        }
+        scanner.close();
     }
 }
